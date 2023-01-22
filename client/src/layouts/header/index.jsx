5cf759f2c0d79df0bea67b7d2481c10a27afbabc
KhidirovaSveta <khidirova.sveta@gmail.com>
2023-01-22 11:40:08 +0400

    added dropdown

--- client/src/layouts/header/index.jsx
+++ client/src/layouts/header/index.jsx
@@ -1,45 +1,78 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Logo from "../../img/logo.png.webp";
+import { Dropdown, Space } from "antd";
 import "./index.scss";
 const Header = () => {
+  const items = [
+    {
+      key: "1",
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="/">
+          Blog
+        </a>
+      ),
+    },
+    {
+      key: "2",
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="/">
+          Blog Details
+        </a>
+      ),
+    },
+    {
+      key: "3",
+      label: (
+        <a target="_blank" rel="noopener noreferrer" href="/">
+          Element
+        </a>
+      ),
+    },
+  ];
   return (
     <div id="Header">
-    <div className="container">
-      <div className="header">
-        <img src={Logo} alt="" />
-        <div className="nav-Bar">
-          <ul>
-            <li>
-              {" "}
-              <NavLink to={"/"}> Home </NavLink>
-            </li>
-            <li>
-              {" "}
-              <NavLink to={"/"}> Courses </NavLink>
-            </li>
-            <li>
-              {" "}
-              <NavLink to={"/"}> About </NavLink>
-            </li>
-            <li>
-              {" "}
-              <NavLink to={"/"}> Blog </NavLink>
-            </li>
-            <li>
-              {" "}
-              <NavLink to={"/"}> Contact </NavLink>
-            </li>
-          </ul>
-          <div className="headerBtn">
-          <button className="btn-join">Join</button>
-          <button className="btn-login">Log In</button>
+      <div className="container">
+        <div className="header">
+          <img src={Logo} alt="" />
+          <div className="nav-Bar">
+            <ul>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Home </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Courses </NavLink>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> About </NavLink>
+              </li>
+              <li>
+                <Dropdown
+                  menu={{
+                    items,
+                  }}
+                >
+                  <a onClick={(e) => e.preventDefault()}>
+                    <Space className="drop">Blog </Space>
+                  </a>
+                </Dropdown>
+              </li>
+              <li>
+                {" "}
+                <NavLink to={"/"}> Contact </NavLink>
+              </li>
+            </ul>
+            <div className="headerBtn">
+              <button className="btn-join">Join</button>
+              <button className="btn-login">Log In</button>
+            </div>
+          </div>
         </div>
-        </div>
-        
       </div>
     </div>
-  </div>
   );
 };
 
